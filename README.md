dd1fc136dca10cb52ef8e1cf9e4aa8586b50ad81
Andrey Karchevsky <62261985+devkarych@users.noreply.github.com>
2022-07-09 20:21:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Aioplate. <a href="https://github.com/devkarych/aioplate/blob/main/DOCS.md">Read the docs</a>
+
+### ⬆ ⬆ ⬆ ⬆ ⬆ ⬆ ⬆ ⬆ ⬆ ⬆
+
+### Below is a universal template for you project description.
+
+# App Name
+
+## App description
+
+...
+
+## Stack
+
+...
+
+## Installation guide
+
+...
+
+## Links
+
+...
+
+## Screenshots & video-previews
+
+...
+
+## Credits
+
+- **App template: <a href="https://github.com/devkarych">Karych</a>.**
---
+++ app/core/handlers/factory.py
@@ -0,0 +1,47 @@
+import types
+from abc import ABC, abstractmethod
+
+from aiogram import Dispatcher
+
+from app.exceptions.handler import RegisterHandlerError
+
+
+class HandlersFactory(ABC):
+    """Declares basic functionality of handlers factory."""
+
+    def __init__(self, dp: Dispatcher) -> None:
+        """
+        :param dp: Dispatcher for providing it to handlers initializers.
+        """
+
+        self._dp = dp
+
+    @abstractmethod
+    def register(self, *handlers) -> None:
+        """Register handlers here..."""
+
+
+class DefaultHandlersFactory(HandlersFactory):
+    """Implements default handlers registration."""
+
+    def register(self, *handlers) -> None:
+        """
+        Handlers registering. If `register_handlers()` wasn't implemented in module,
+        it skips with error log message.
+        :param handlers: .py handler-modules with implemented register_handlers() method.
+        """
+
+        for handler in handlers:
+            if isinstance(handler, types.ModuleType):
+                try:
+                    handler.register_handlers(dp=self._dp)
+                except AttributeError as error:
+                    raise RegisterHandlerError(
+                        f"register_handlers() method wasn't implemented "
+                        f"in {str(error.obj)}"
+                    )
+            else:
+                raise RegisterHandlerError(
+                    f"{handler} from submitted args to `register_handlers()` "
+                    f"is not a .py module"
+                )
